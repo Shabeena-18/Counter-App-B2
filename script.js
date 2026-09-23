d3e4289286bb0fcdcd2d05a10c99695a23580234
Shabeena-18 <shaik.shabeena888888@gmail.com>
2026-09-23 17:37:44 +0530

    Initialized counter project

---
+++ script.js
@@ -0,0 +1,55 @@
+console.log("script");
+let count = document.getElementById("counter");
+console.log(count.textContent, typeof(count.textContent));
+
+function Increment(){
+    console.log("Clicked");
+    let previousCount = parseInt(count.textContent);
+    console.log(previousCount);
+    let updateCount = previousCount + 1;
+    console.log(updateCount);
+    count.textContent = updateCount;
+   if(updateCount>0){
+    count.style.color = "green";
+    console.log(count);
+   }
+   else if(updateCount<0){
+    count.style.color = "red";
+   }
+   else{
+    count.style.color = "black";
+   }
+}
+
+function Decrement(){
+    console.log("Clicked");
+    let previousCount = parseInt(count.textContent);
+    console.log(previousCount);
+    let updateCount = previousCount - 1;
+    console.log(updateCount);
+    count.textContent = updateCount;
+    if(updateCount>0){
+    count.style.color = "green";
+    console.log(count);
+   }
+   else if(updateCount<0){
+    count.style.color = "red";
+   }
+   else{
+    count.style.color = "black";
+   }
+}
+
+function Reset(){
+    count.textContent = parseInt(0);
+    if(updateCount>0){
+    count.style.color = "green";
+    console.log(count);
+   }
+   else if(updateCount<0){
+    count.style.color = "red";
+   }
+   else{
+    count.style.color = "black";
+   }
+}
